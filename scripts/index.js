15a6ccccdc6b8a1f032f51b296de1d5708ce9263
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-01 03:34:55 +0100

    Linked the sites with ID

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
 let userName = prompt("What is your name:");
 alert(`Hello ${userName}, 
 Welcome!!!
- This page is Still under construction and it is being developred by TonniPaul for practice reason.`);
+This page is Still under construction and it is being developed by TonniPaul for practice reasons.`);
