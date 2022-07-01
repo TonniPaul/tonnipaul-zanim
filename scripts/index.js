a02bd5bc925365ee025ce32f78e2e89b594201dc
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-01 03:01:52 +0100

    Uploaded Java script

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+let userName = prompt("What is your name:");
+alert(`Hello ${userName}, 
+Welcome!!!
+ This page is Still under construction and it is being developred by TonniPaul for practice reason.`);
