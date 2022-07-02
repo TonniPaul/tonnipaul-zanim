412384eb2a7470aed34944de0d5a43c69f50dd98
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-02 15:08:52 +0100

    creating mobile view

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
 let userName = prompt("What is your name:");
 alert(`Hello ${userName}, 
 Welcome!!!
-This page is Still under construction and it is being developed by TonniPaul for practice reasons.`);
+
+This page is Still under construction!!.
+   =================================
+Developed by TonniPaul for practice reasons.
+
+`);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () =>{
+   hamburger.classList.toggle("active");
+   navMenu.classList.toggle("active");
+})
