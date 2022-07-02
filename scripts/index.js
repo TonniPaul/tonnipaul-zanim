1a0cf842bce0523db9c720fb52eb82af0526064c
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-02 19:19:53 +0100

    updated mobile view and tab view

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,15 @@ Developed by TonniPaul for practice reasons.
 
 `);
 
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector("hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () =>{
    hamburger.classList.toggle("active");
    navMenu.classList.toggle("active");
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+   hamburger.classList.remove("active");
+   navMenuclassList.remove("active");
+}))
