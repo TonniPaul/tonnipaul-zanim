815852819a5afcdade8cff50f07708d03a71d5d9
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-03 20:53:46 +0100

    Added javascript and changed a few things in mobile view

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,54 @@ This page is Still under construction!!.
 Developed by TonniPaul for practice reasons.
 
 `);
+const menuBtn = document.querySelector('.hmb');
+let menuOpen = false;
+let navMenu = document.querySelector('.nav-menu');
 
-const hamburger = document.querySelector("hamburger");
-const navMenu = document.querySelector(".nav-menu");
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    navMenu= navMenu.classList.add('active');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
 
-hamburger.addEventListener("click", () =>{
-   hamburger.classList.toggle("active");
-   navMenu.classList.toggle("active");
+let link = document.querySelector('nav-link');
+link.addEventListener('click', () => {
+   menuBtn.classList.remove('open');
+   navMenu= navMenu.classList.remove('active');
+   menuOpen = false;
 })
-
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
-   hamburger.classList.remove("active");
+   menuBtn.classList.remove("open");
    navMenuclassList.remove("active");
 }))
+
+// if(menuBtn.classList('open'))  {
+   
+//    menuOpen= true
+// }else{
+   
+//    menuOpen = false;
+// };
+
+
+
+
+
+
+// const hamburger = document.querySelector("hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () =>{
+//    hamburger.classList.toggle("active");
+//    navMenu.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+//    hamburger.classList.remove("active");
+//    navMenuclassList.remove("active");
+// }))
--- scripts/login.js
+++ scripts/login.js
@@ -2,10 +2,10 @@ const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
 
 togglePassword.addEventListener("click", function () {
-    // toggle the type attribute
+    /* toggle the type attribute */
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
     
-    // toggle the icon
+    /* toggle the icon */
     this.classList.toggle("bi-eye");
 });
